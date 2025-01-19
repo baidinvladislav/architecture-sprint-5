f77c1d9fe677e56daf7dddafc3e57ecca711ccce
baydinvladislav <baydinvladislav@yandex.ru>
2025-01-19 14:49:39 +0100

    add rasa models

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,7 @@ terraform {
 
 provider "yandex" {
   folder_id = "b1gpijacueemuiv6g27k"
+  token     = "t1.9euelZqWx5OemcmRx8vOzsaZiZDPzu3rnpWalJ6YkZvGnJScnp6ciZKVmZjl8_dKJ0xD-e8gNkoL_t3z9wpWSUP57yA2Sgv-zef1656VmszNm8qLzImMzcqcnIyezMqS7_zN5_XrnpWax5POx5mNypqYjoyLjsvNm4rv_cXrnpWazM2byovMiYzNypycjJ7MypI.aYKOV5CRlACHWZymIqamp3H8UW9ClVqfW0pfj9TLWEAC0w9T6J2h8yav5DR4kuNYCMBPy4_Bf4EMdnJWECAeBA"
 }
 
 data "yandex_compute_image" "ubuntu" {
@@ -16,7 +17,7 @@ data "yandex_compute_image" "ubuntu" {
 }
 
 resource "yandex_compute_instance" "vm" {
-  name = "test-vm"
+  name = "terraform-instance"
   zone = "ru-central1-b"
 
   resources {
@@ -38,5 +39,36 @@ resource "yandex_compute_instance" "vm" {
 
   metadata = {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    user-data = <<-EOT
+      #cloud-config
+      package_update: true
+      package_upgrade: true
+      packages:
+        - python3
+        - python3-pip
+        - build-essential
+        - libssl-dev
+        - libffi-dev
+        - python3-dev
+        - curl
+        - git
+        - nodejs
+        - npm
+      runcmd:
+        - echo "Attempting to clone repository" > /home/ubuntu/clone_debug.log
+        - apt-get update && apt-get install -y git >> /home/ubuntu/debug.log 2>&1
+        - git clone --branch sprint-5 https://github.com/baidinvladislav/architecture-sprint-5.git /home/ubuntu/project >> /home/ubuntu/debug.log 2>&1
+        - cd /home/ubuntu
+        - pip3 install virtualenv
+        - virtualenv /opt/rasa_env
+        - /opt/rasa_env/bin/pip install rasa transformers
+        - /opt/rasa_env/bin/rasa train
+        - /opt/rasa_env/bin/rasa run actions
+        - /opt/rasa_env/bin/rasa run --cors "*"
+        - npm install
+        - npm run build
+        - npm run start
+        - echo "Project backend and frontend are running!" > /root/project_status.txt
+    EOT
   }
 }
